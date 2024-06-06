6cc839ee8c4c44cfd1a34d184a9a1c2349251539
bhusandas <sonubhusan98@gmail.com>
2024-06-06 13:33:05 +0530

    Added booking model

---
+++ utils/errors/service-error.js
@@ -0,0 +1,21 @@
+import {
+	ReasonPhrases,
+	StatusCodes,
+	getReasonPhrase,
+	getStatusCode,
+} from 'http-status-codes';
+
+class ServiceError extends Error{
+    constructor(
+        message = 'Something Went Wrong',
+        explanation = 'Service layer error',
+        statusCodes = StatusCodes.INTERNAL_SERVER_ERROR
+    ){
+        super();
+        this.message = message;
+        this.explanation = explanation;
+        this.statusCodes = statusCodes;
+    }
+}
+
+module.exports = ServiceError;
